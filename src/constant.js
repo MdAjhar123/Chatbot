726f6f367c11437609b19ef6969396dcef55a6e1
MD AJHAR <114383811+MdAjhar123@users.noreply.github.com>
2026-09-08 13:02:17 +0530

    Update URL to use gemini-3.5-flash-lite model

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
-export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`
+export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent?key=${API_KEY}`
 
 // export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=YOUR_API_KEY";
 
@@ -8,5 +8,5 @@ export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemi
 //BELOW ARE DIFFERENT GEMINI MODELS
 // gemini-flash-latest
 // gemini-3-flash-preview  *
-// gemini-3.5-flash-lite //old reply more response but not latest information
+// gemini-3.5-flash-lite //new reply more response but not latest information
 // gemini-3.1-pro-preview
